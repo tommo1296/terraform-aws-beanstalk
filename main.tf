90a28f391ed2b5d9777a685d96143b5ef16aa317
Thomas Altree <altree.thomas@googlemail.com>
2021-05-18 09:06:00 +0100

    Add environment resources

--- main.tf
+++ main.tf
@@ -9,3 +9,26 @@ resource "aws_elastic_beanstalk_application" "this" {
 
   tags = var.tags
 }
+
+resource "aws_elastic_beanstalk_environment" "this" {
+  name                  = var.environment_name
+  description           = var.environment_description
+  application           = aws_elastic_beanstalk_application.this.name
+  cname_prefix          = var.environment_cname_prefix
+
+  solution_stack_name   = var.solution_stack_name
+
+  wait_for_ready_timeout = var.environment_timeout
+
+  tags = var.tags
+
+  dynamic "setting" {
+    for_each = var.settings
+
+    content {
+      namespace   = setting.value["namespace"]
+      name        = setting.value["name"]
+      value       = setting.value["value"]
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -10,6 +10,18 @@ variable "application_description" {
   default       = ""
 }
 
+variable "environment_name" {
+  type          = string
+  description   = "What environment dev, stage etc... Must be unique within the application"
+  default       = ""
+}
+
+variable "environment_description" {
+  type          = string
+  description   = "Description of the environment"
+  default       = ""
+}
+
 variable "application_service_role_arn" {
   type          = string
   description   = "The service role ARN with permissions to manage beanstalk resources"
@@ -26,4 +38,28 @@ variable "tags" {
   type          = map(string)
   description   = "Application specific tags"
   default       = {}
+}
+
+variable "environment_cname_prefix" {
+  type          = string
+  description   = "Add a custom cname for the app"
+  default       = ""
+}
+
+variable "solution_stack_name" {
+  type          = string
+  description   = "Stack solutin name (See Beanstalk supported platforms documentation)"
+  default       = ""
+}
+
+variable "environment_timeout" {
+  type          = string
+  description   = "Time terraform waits for environment to start"
+  default       = "20m"
+}
+
+variable "settings" {
+  type          = list(map(string))
+  description   = "List of maps for the configuration options"
+  default       = []
 }
